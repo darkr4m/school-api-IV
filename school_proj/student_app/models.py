0250468402b4d7c4a17ec2e27efc21047281634d
max <hmwright404@gmail.com>
2025-03-26 15:08:49 -0500

    add serializers

--- school_proj/student_app/models.py
+++ school_proj/student_app/models.py
@@ -7,7 +7,10 @@ from .validators import validate_name_format, validate_email_format, validate_lo
 class Student(models.Model):
     name = models.CharField(max_length=255, null = False, blank = False, validators=[validate_name_format])
     student_email = models.EmailField(null = False, blank = False, unique=True, validators=[validate_email_format])
-    personal_email = models.EmailField(null = False, blank = False, unique=True)
+    personal_email = models.EmailField(blank = True, unique=True)
     locker_number = models.IntegerField(default = 110, null = False, blank = False, unique=True, validators=[v.MinValueValidator(1), v.MaxValueValidator(200)])
     locker_combination = models.CharField( default="12-12-12",null = False, blank = False, max_length=10, validators=[validate_locker_combination])
     good_student = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.name}"
